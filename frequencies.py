d9b2740118858e0c31473e6260bea18b868e0e11
Fardeen Shah Bin Idrus <fardeen.idrus@kcl.ac.uk>
2023-09-28 12:00:07 +0100

    init

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,18 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    counter = 0
+    for i in items:
+        i = str(i)
+        for item in items:
+            item = str(item)
+            if item == i:
+                counter +=1
+        frequencies.update({i: counter})
+        counter = 0
     return frequencies
+
+
+# testList = ['a','a','b','c']
+# print(frequencies(testList))
+
